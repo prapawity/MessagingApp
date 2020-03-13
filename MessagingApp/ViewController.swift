f68f0cc5e5603d12bf1eed9252a578b87aaba601
prapawity <protfc16@gmail.com>
2020-03-13 12:11:05 +0700

    ✅ init project

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Prapawit Patthasirivichot on 11/3/2563 BE.
+//  Copyright © 2563 Prapawit Patthasirivichot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
