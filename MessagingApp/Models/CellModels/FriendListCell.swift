117e51b6a97e0808a3ad38c1af09d9ff32db05b7
prapawity <protfc16@gmail.com>
2020-03-15 10:33:57 +0700

    ✅ add TableView all page and embed class

---
+++ MessagingApp/Models/CellModels/FriendListCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendListCell.swift
+//  MessagingApp
+//
+//  Created by Prapawit Patthasirivichot on 14/3/2563 BE.
+//  Copyright © 2563 Prapawit Patthasirivichot. All rights reserved.
+//
+
+import UIKit
+
+class FriendListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
